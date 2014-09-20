063c2cc97905bb26d30e161fbe817baf09ae684b
Cameron Knight <cameron@wirestorm.us>
2014-09-19 22:43:10 -0400

    add describeEach spec helper

--- app/test-main.js
+++ app/test-main.js
@@ -1,42 +1,56 @@
-(function() {
-    "use strict";
+(function () {
+  /*jshint freeze:false*/
+  /*global requirejs:false*/
+  "use strict";
 
-    var tests = [];
-    for (var file in window.__karma__.files) {
+  var tests = [];
+  for (var file in window.__karma__.files) {
 
-        if (window.__karma__.files.hasOwnProperty(file)) {
-            if (/^\/base\/app\/spec\//i.test(file) && /Spec\.js$/.test(file)) {
-                tests.push(file);
-            }
-        }
+    if (window.__karma__.files.hasOwnProperty(file)) {
+      if (/^\/base\/app\/spec\//i.test(file) && /Spec\.js$/.test(file)) {
+        tests.push(file);
+      }
     }
+  }
 
-    var require_config = {
-        "baseUrl": "/base/app",
-        "deps": ["angular", "angular-mocks", "sinon", "angular-resource", "jasmine-sinon"],
-        "paths": {
-            "angular": "vendor/angular/angular",
-            "angular-ui-router": "vendor/angular-ui-router/angular-ui-router",
-            "angular-resource": "vendor/angular-resource/angular-resource",
-            "angular-mocks": "vendor/angular-mocks/angular-mocks",
-            "sinon": "vendor/sinon/lib/sinon",
-            "jasmine-sinon": "vendor/jasmine-sinon/lib/jasmine-sinon"
-        },
-        "shim": {
-            "angular-mocks": ["angular"],
-            "angular-resource": ["angular"],
-            "angular-ui-router": ["angular"]
-        }
-    };
+  var calculateBeforeEachTitle = function (title, item) {
+    return ('' + title)
+      .replace(/\{value\}/ig, '' + item);
+  };
+  window.describeEach = function (title, items, callback) {
+    items.forEach(function (item) {
+      describe(calculateBeforeEachTitle(title, item), function () {
+        return callback.call(this, item);
+      });
+    });
+  };
 
-    require_config["callback"] = function() { /*jshint ignore:line */
-        requirejs(["src/app"], function() {
-            Object.prototype["@" + Math.random()] = null;
-            requirejs(tests, function() {
-                window.__karma__.start();
-            });
-        });
-    };
+  var require_config = {
+    "baseUrl": "/base/app",
+    "deps": ["angular", "angular-mocks", "sinon", "angular-resource", "jasmine-sinon"],
+    "paths": {
+      "angular": "vendor/angular/angular",
+      "angular-ui-router": "vendor/angular-ui-router/angular-ui-router",
+      "angular-resource": "vendor/angular-resource/angular-resource",
+      "angular-mocks": "vendor/angular-mocks/angular-mocks",
+      "sinon": "vendor/sinon/lib/sinon",
+      "jasmine-sinon": "vendor/jasmine-sinon/lib/jasmine-sinon"
+    },
+    "shim": {
+      "angular-mocks": ["angular"],
+      "angular-resource": ["angular"],
+      "angular-ui-router": ["angular"]
+    }
+  };
 
-    requirejs.config(require_config);
+  require_config["callback"] = function () { /*jshint ignore:line */
+    requirejs(["src/app"], function () {
+      Object.prototype["@" + Math.random()] = null;
+      requirejs(tests, function () {
+        window.__karma__.start();
+      });
+    });
+  };
+
+  requirejs.config(require_config);
 }());
